44bb915fdcc6d42048ad3d34d17558008a9e1d57
june960427 <ljh975896@gmail.com>
2024-09-01 16:36:28 +0900

    add : api 초기설정 완료 plan : 각 api 호출에 필요한 포맷 시간 추가 예정

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -1,22 +1,24 @@
 import axios from 'axios'
+import { formattedDate, formattedHour } from '@utils/FormattedTime'
 
 const API_KEY = import.meta.env.VITE_API_KEY
 const BASE_URL = 'https://apis.data.go.kr/'
-const WEATHER_ENDPOINT = '1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'
-const AIR_POLLUTION_ENDPOINT = 'B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty'
-
-const formatDate = (date = new Date()) => {
-  return date.toISOString().slice(0, 10).replace(/-/g, '')
-}
+const 초단기실황조회_ENDPOINT = '1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'
+const 초단기예보조회_ENDPOINT = '1360000/VilageFcstInfoService_2.0/getUltraSrtFcst'
+const 단기예보조회_ENDPOINT = '1360000/VilageFcstInfoService_2.0/getVilageFcst'
 
 const createUrl = (endpoint, params = {}) => {
   const url = new URL(endpoint, BASE_URL)
   const searchParams = new URLSearchParams({
     serviceKey: decodeURIComponent(API_KEY),
     returnType: 'json',
+    pageNo: '1',
+    numOfRows: '1000',
+    base_date: formattedDate(),
     ...params,
   })
   url.search = searchParams.toString()
+
   return url.toString()
 }
 
@@ -25,38 +27,49 @@ const handleApiError = (error, message) => {
   throw new Error(message)
 }
 
-const weatherRequest = async (time = '2400', nx = 127, ny = 67) => {
+const 초단기실황조회 = async (nx, ny) => {
+  // 06시 발표(정시단위)
   try {
-    const url = createUrl(WEATHER_ENDPOINT, {
-      pageNo: '1',
-      numOfRows: '1000',
-      dataType: 'json',
-      base_date: formatDate(),
-      base_time: time,
+    const url = createUrl(초단기실황조회_ENDPOINT, {
+      base_time: formattedHour(),
       nx: nx.toString(),
       ny: ny.toString(),
     })
-
     const response = await axios.get(url)
     return response.data
-  } catch (error) {
-    handleApiError(error, '날씨 정보를 가져오는 중 오류가 발생했습니다')
+  } catch (err) {
+    handleApiError(err, '초단기실황조회 API 호출 오류')
   }
 }
 
-const airPollutionRequest = async (region = '서울') => {
+const 초단기예보조회 = async (nx, ny) => {
+  // 06시30분 발표(30분 단위)
   try {
-    const url = createUrl(AIR_POLLUTION_ENDPOINT, {
-      numOfRows: '100',
-      pageNo: '1',
-      sidoName: region,
-      ver: '1.0',
+    const url = createUrl(초단기예보조회_ENDPOINT, {
+      base_time: formattedHour(),
+      nx: nx.toString(),
+      ny: ny.toString(),
     })
     const response = await axios.get(url)
-    return response.data.response.body.items
-  } catch (error) {
-    handleApiError(error, '미세먼지 정보를 가져오는 중 오류가 발생했습니다')
+    return response.data
+  } catch (err) {
+    handleApiError(err, '초단기예보조회 API 호출 오류')
   }
 }
 
-export default { weatherRequest, airPollutionRequest }
+const 단기예보조회 = async (nx, ny) => {
+  // 05시 발표
+  try {
+    const url = createUrl(단기예보조회_ENDPOINT, {
+      base_time: formattedHour(),
+      nx: nx.toString(),
+      ny: ny.toString(),
+    })
+    const response = await axios.get(url)
+    return response.data
+  } catch (err) {
+    handleApiError(err, '단기예보조회 API 호출 오류')
+  }
+}
+
+export { 초단기실황조회, 초단기예보조회, 단기예보조회 }
--- src/utils/FormattedTime.js
+++ src/utils/FormattedTime.js
@@ -2,7 +2,7 @@ const formattedDate = () => {
   const date = new Date()
   const year = date.getFullYear().toString()
   const month = (date.getMonth() + 1).toString().padStart(2, '0')
-  const day = date.getDate().toString().padStart(3, '0')
+  const day = date.getDate().toString().padStart(2, '0')
 
   return year + month + day
 }
@@ -12,7 +12,7 @@ const formattedHour = () => {
   const hour = date.getHours()
   const foramtHour = hour.toString().padStart(2, '0')
 
-  return hour
+  return foramtHour
 }
 
-export default { formattedDate, formattedHour }
+export { formattedDate, formattedHour }
