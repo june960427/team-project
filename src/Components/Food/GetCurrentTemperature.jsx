46697d2857dd4a9513446e574d73547d4020f6a9
ahcgnoej <jinjinchachajung@naver.com>
2024-09-09 18:01:16 +0900

    Feat : 기온에 따른 음식 추천

---
+++ src/Components/Food/GetCurrentTemperature.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { 단기예보조회 } from '../../apis/apis.js'
+
+const GetCurrentTemperature = ({ onTemperatureUpdate }) => {
+  const [temperature, setTemperature] = useState(null)
+  const NX = 61
+  const NY = 126
+
+  const filterByCategory = (category) => (data) => data.category === category
+  const formatForecastTime = (time) => time.slice(0, 2)
+  const foramtValueName = ({ fcstTime, fcstValue, ...rest }) => ({
+    시간: formatForecastTime(fcstTime),
+    온도: fcstValue,
+    ...rest,
+  })
+  const getLatestItem = (items) => {
+    if (items.length === 0) return null
+    const sortedItems = items.sort((a, b) => new Date(`${a.날짜} ${a.시간}`) - new Date(`${b.날짜} ${b.시간}`))
+    return sortedItems[sortedItems.length - 1]
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await 단기예보조회(NX, NY)
+        console.log('API 응답:', response)
+
+        if (response.data.response.body.items) {
+          const items = response.data.response.body.items.item.filter(filterByCategory('TMP')).map(foramtValueName)
+          const latestItem = getLatestItem(items)
+          if (latestItem) {
+            setTemperature(latestItem.온도)
+            onTemperatureUpdate(latestItem.온도)
+          } else {
+            throw new Error('데이터가 없습니다.')
+          }
+        } else {
+          throw new Error('API 응답 형식이 잘못되었습니다.')
+        }
+      } catch (err) {
+        setError('온도 조회 중 오류 발생: ' + err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [onTemperatureUpdate])
+}
+
+export default GetCurrentTemperature
