abc00c5b6cc9e0a6e983233d9ba65db7e0c06d78
eunjju2 <graceun309@gmail.com>
2024-09-09 20:39:41 +0900

    Feat: 현재, 최고 및 최저 기온 시각화 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from '@components/Header/Header'
+import MinMaxTemp from '@components/MinMaxTemp/MinMaxTemp'
 
 const App = () => {
   return (
     <>
       <Header />
+      <MinMaxTemp />
     </>
   )
 }
